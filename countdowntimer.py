a0ed9d9d3cd59656afb5d75acae96b9ec5b7a9a1
sree34u <173282444+sree34u@users.noreply.github.com>
2025-10-17 15:10:09 +0000

    Update countdown timer

--- countdowntimer.py
+++ countdowntimer.py
@@ -3,8 +3,8 @@ import time
 my_time = int(input("Enter the time in seconds:"))
 for x in range(my_time,0,-1):
     sec = x % 60
-    min = int(x/60)%60
-    hrs = int(x/3600)
-    print(f"{hrs:02}:{min:02}:{sec:02}")
+    min = (x//60)%60
+    hrs = x//3600
+    print(f"{hrs:02}:{min:02}:{sec:02}",end="\r")
     time.sleep(1)
-print("time's up")
+print("BOOM!!!💥")
